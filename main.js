838eeee42c677ce45f3319f063868f0d9cd7f438
MathiasM <mamu@step.dk>
2017-08-07 12:57:27 +0200

    Created project Server set-up Following basic example from phaser-docs

---
+++ main.js
@@ -0,0 +1,12 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+  game.load.image('guy', 'assets/guy.png');
+}
+
+function create() {
+  game.add.sprite(0, 0, 'guy');
+}
+
+function update() {
+}
