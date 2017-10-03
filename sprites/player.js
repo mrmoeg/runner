5365e199cbdb7639660a379382fa0e07f1ac0bfe
Mamu <mathiasmun@gmail.com>
2017-10-03 11:18:48 +0200

    Added an Air Meter. Started working on blowing particles

--- sprites/player.js
+++ sprites/player.js
@@ -1,3 +1,10 @@
+//       :::::::::  :::            :::   :::   ::: :::::::::: :::::::::    ::: :::
+//      :+:    :+: :+:          :+: :+: :+:   :+: :+:        :+:    :+:  :+:   :+:
+//     +:+    +:+ +:+         +:+   +:+ +:+ +:+  +:+        +:+    +:+ +:+     +:+
+//    +#++:++#+  +#+        +#++:++#++: +#++:   +#++:++#   +#++:++#:  +#+     +#+
+//   +#+        +#+        +#+     +#+  +#+    +#+        +#+    +#+ +#+     +#+
+//  #+#        #+#        #+#     #+#  #+#    #+#        #+#    #+#  #+#   #+#
+// ###        ########## ###     ###  ###    ########## ###    ###   ### ###
 function Player( ) {
   this.deltaTime = 0;
   this.height    = 32;
@@ -16,8 +23,16 @@ function Player( ) {
   this.cooldownTimer = false;
 }
 
+//       ::::::::::: ::::    ::: ::::::::::: :::::::::::
+//          :+:     :+:+:   :+:     :+:         :+:
+//         +:+     :+:+:+  +:+     +:+         +:+
+//        +#+     +#+ +:+ +#+     +#+         +#+
+//       +#+     +#+  +#+#+#     +#+         +#+
+//      #+#     #+#   #+#+#     #+#         #+#
+// ########### ###    #### ###########     ###
 Player.prototype.init = function(x,y){
   this.point = new Phaser.Point(x,y);
+  this.createAirMeter();
 
   this.sprite = game.add.sprite(this.point.x, this.point.y, "player_img");
   this.sprite.anchor.setTo(0.5,0.5);
@@ -29,9 +44,16 @@ Player.prototype.init = function(x,y){
 
 Player.prototype.preload = function(){
   game.load.image('player_img', 'assets/player.png');
+  game.load.image('blowParticle', 'assets/particles/blow.png');
 };
 
-
+//      :::    ::: :::::::::  :::::::::      ::: ::::::::::: ::::::::::
+//     :+:    :+: :+:    :+: :+:    :+:   :+: :+:   :+:     :+:
+//    +:+    +:+ +:+    +:+ +:+    +:+  +:+   +:+  +:+     +:+
+//   +#+    +:+ +#++:++#+  +#+    +:+ +#++:++#++: +#+     +#++:++#
+//  +#+    +#+ +#+        +#+    +#+ +#+     +#+ +#+     +#+
+// #+#    #+# #+#        #+#    #+# #+#     #+# #+#     #+#
+// ########  ###        #########  ###     ### ###     ##########
 Player.prototype.update = function(){
   this.deltaTime = game.time.elapsed/1000;
   this.canBlow = this.air > 0;
@@ -42,16 +64,19 @@ Player.prototype.update = function(){
     this.break();
     this.charge();
   }
-};
+}; // update()
 
 Player.prototype.boost = function( ){
   this.isCharging = false;
   var angle = Math.atan2(this.sprite.body.position.y - game.input.y, this.sprite.body.position.x - game.input.x);
-  this.sprite.body.velocity.y += Math.sin(angle) * this.force;
-  this.sprite.body.velocity.x += Math.cos(angle) * this.force;
+  var angleX = Math.sin(angle);
+  var angleY = Math.cos(angle);
+  this.sprite.body.velocity.y += angleX * this.force;
+  this.sprite.body.velocity.x += angleY * this.force;
 
   this.air -= this.blowRate * this.deltaTime;
-};
+  // this.emitBlow(angleX, angleY);
+}; // boost()
 
 Player.prototype.break = function( ){
   this.sprite.body.drag.x = Math.abs( this.sprite.body.velocity.x ) * this.drag;
@@ -59,35 +84,96 @@ Player.prototype.break = function( ){
   if( !this.cooldownTimer && this.air < this.airMax && !this.isCharging ){
     this.cooldownTimer = game.time.events.add(Phaser.Timer.SECOND * this.airCooldown, this.startCharging, this);
   }
-};
+}; // break()
 
 Player.prototype.startCharging = function(){
   this.cooldownTimer = false;
   this.isCharging = true;
-};
+}; // startCharging()
 Player.prototype.charge = function(){
   if(this.isCharging && this.air < this.airMax){
     this.air += this.chargeRate * this.deltaTime;
   }
-};
+}; // charge()
+//           :::     ::::::::::: :::::::::    :::   :::   :::::::::: ::::::::::: :::::::::: :::::::::
+//        :+: :+:       :+:     :+:    :+:  :+:+: :+:+:  :+:            :+:     :+:        :+:    :+:
+//      +:+   +:+      +:+     +:+    +:+ +:+ +:+:+ +:+ +:+            +:+     +:+        +:+    +:+
+//    +#++:++#++:     +#+     +#++:++#:  +#+  +:+  +#+ +#++:++#       +#+     +#++:++#   +#++:++#:
+//   +#+     +#+     +#+     +#+    +#+ +#+       +#+ +#+            +#+     +#+        +#+    +#+
+//  #+#     #+#     #+#     #+#    #+# #+#       #+# #+#            #+#     #+#        #+#    #+#
+// ###     ### ########### ###    ### ###       ### ##########     ###     ########## ###    ###
+Player.prototype.createAirMeter = function(){
+  this.airMeterRect = new Phaser.Rectangle( 0, 0, this.width, this.width/5 ) ;
+  this.airMeterBackRect = new Phaser.Rectangle( 0, 0, this.width, this.width/5 ) ;
+}; // createAirMeter()
+Player.prototype.drawAirMeter = function(){
+  var airMeterColor = "rgba(255,0,0,1)";
+  if(this.air > this.airMax / 2){
+    airMeterColor = "rgba(0,255,0,1)";
+  }else if(this.air > this.airMax / 5){
+    airMeterColor = "rgba(255,155,0,1)";
+  }
+  this.airMeterRect.x = this.sprite.body.x;
+  this.airMeterRect.y = this.sprite.body.y-this.height/2 - this.width/5;
+  this.airMeterRect.width = this.width * ( this.air/this.airMax);
+  this.airMeterBackRect.x = this.airMeterRect.x;
+  this.airMeterBackRect.y = this.airMeterRect.y;
+  game.debug.geom( this.airMeterBackRect, "rgba(160,160,160,1)" ) ;
+  game.debug.geom( this.airMeterRect, airMeterColor ) ;
+}; // drawAirMeter()
 
+//       :::::::::  :::        ::::::::  :::       ::: ::::::::::   :::   :::   ::::::::::: ::::::::::: ::::::::::: :::::::::: :::::::::
+//      :+:    :+: :+:       :+:    :+: :+:       :+: :+:         :+:+: :+:+:      :+:         :+:         :+:     :+:        :+:    :+:
+//     +:+    +:+ +:+       +:+    +:+ +:+       +:+ +:+        +:+ +:+:+ +:+     +:+         +:+         +:+     +:+        +:+    +:+
+//    +#++:++#+  +#+       +#+    +:+ +#+  +:+  +#+ +#++:++#   +#+  +:+  +#+     +#+         +#+         +#+     +#++:++#   +#++:++#:
+//   +#+    +#+ +#+       +#+    +#+ +#+ +#+#+ +#+ +#+        +#+       +#+     +#+         +#+         +#+     +#+        +#+    +#+
+//  #+#    #+# #+#       #+#    #+#  #+#+# #+#+#  #+#        #+#       #+#     #+#         #+#         #+#     #+#        #+#    #+#
+// #########  ########## ########    ###   ###   ########## ###       ### ###########     ###         ###     ########## ###    ###
+// Player.prototype.createBlowEmitter = function(){
+//   this.blowEmitter = game.add.emitter(this.sprite.body.x, this.sprite.body.y, this.height, this.width);
+//   this.blowEmitter.particleSpeedMin = 0.01;
+//   this.blowEmitter.particleSpeedMax = 0.3;
+//   this.blowEmitter.makeParticles( "blowParticle" );
+//   return this.blowEmitter;
+// };
+// Player.prototype.emitBlow = function(angleX, angleY){
+//   this.blowEmitter = ( typeof this.blowEmitter === "undefined")?this.createBlowEmitter():this.blowEmitter;
+//   this.blowEmitter.centerX = this.sprite.body.x + this.width/2;
+//   this.blowEmitter.centerY = this.sprite.body.y + this.height/2;
+//   this.blowEmitter.setXSpeed(this.blowEmitter.particleSpeedMin * angleX, this.blowEmitter.particleSpeedMax * angleX);
+//   this.blowEmitter.setYSpeed(this.blowEmitter.particleSpeedMin * angleY, this.blowEmitter.particleSpeedMax * angleY);
+//   // for (var i = 0; i < Math.ceil(Math.random() * 5); i++) {
+//   //   this.blowEmitter.emitParticle();
+//   // }
+//   this.blowEmitter.emitParticle();
+//   // this.blowEmitter.explode(200,10);
+// };
+
+//       :::::::::  :::::::::: ::::    ::: :::::::::  :::::::::: :::::::::
+//      :+:    :+: :+:        :+:+:   :+: :+:    :+: :+:        :+:    :+:
+//     +:+    +:+ +:+        :+:+:+  +:+ +:+    +:+ +:+        +:+    +:+
+//    +#++:++#:  +#++:++#   +#+ +:+ +#+ +#+    +:+ +#++:++#   +#++:++#:
+//   +#+    +#+ +#+        +#+  +#+#+# +#+    +#+ +#+        +#+    +#+
+//  #+#    #+# #+#        #+#   #+#+# #+#    #+# #+#        #+#    #+#
+// ###    ### ########## ###    #### #########  ########## ###    ###
 Player.prototype.render = function(){
-  // game.debug.geom(this.point, 'rgb(0,255,0)');
-  var debugstr = "Air: "+this.air;
-  game.debug.text(debugstr, 10, 12, 'rgb(255,0,255)');
+  this.drawAirMeter();
 
-  debugstr = "canBlow: "+this.canBlow;
-  game.debug.text(debugstr, 10, 24, 'rgb(255,0,255)');
-
-  debugstr = "isCharging: "+this.isCharging;
-  game.debug.text(debugstr, 10, 36, 'rgb(255,0,255)');
-
-  debugstr = "airCooldown: "+this.airCooldown;
-  game.debug.text(debugstr, 10, 48, 'rgb(255,0,255)');
-
-  debugstr = "chargeRate: "+this.chargeRate;
-  game.debug.text(debugstr, 10, 60, 'rgb(255,0,255)');
-
-  debugstr = "blowRate: "+this.blowRate;
-  game.debug.text(debugstr, 10, 72, 'rgb(255,0,255)');
-};
+  // var debugstr = "Air: "+this.air;
+  // game.debug.text(debugstr, 10, 12, 'rgb(255,0,255)');
+  //
+  // debugstr = "canBlow: "+this.canBlow;
+  // game.debug.text(debugstr, 10, 24, 'rgb(255,0,255)');
+  //
+  // debugstr = "isCharging: "+this.isCharging;
+  // game.debug.text(debugstr, 10, 36, 'rgb(255,0,255)');
+  //
+  // debugstr = "airCooldown: "+this.airCooldown;
+  // game.debug.text(debugstr, 10, 48, 'rgb(255,0,255)');
+  //
+  // debugstr = "chargeRate: "+this.chargeRate;
+  // game.debug.text(debugstr, 10, 60, 'rgb(255,0,255)');
+  //
+  // debugstr = "blowRate: "+this.blowRate;
+  // game.debug.text(debugstr, 10, 72, 'rgb(255,0,255)');
+}; // render()
